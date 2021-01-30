09306ab0aaa1ba8a84fca73781aef746334d6fc2
hanif9050 <hanic.ctg9050@gmail.com>
2021-01-30 19:09:12 +0600

    update Version

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-const firstClassIncreased = document.getElementById('increase-f-class');
+// first class increased button
+const firstClassIncreased = getElementById('increase-f-class');
 firstClassIncreased.addEventListener("click", function() {
-
-    // const fClassCount = document.getElementById("f-class-count").value;
-    // const fClassCountNumber = parseInt(fClassCount);
-    // const countIncrease = fClassCountNumber + 1;
-    // document.getElementById("f-class-count").value = countIncrease;
-    count("f-class-count", true);
-
+    count("f-class-count", true); //count function true increased by one
     const countForMultiply = getElementByIdWithValue("f-class-count");
     const price = 150 * countForMultiply;
     const updatePrice = getElementById("f-class-price")
@@ -14,26 +9,18 @@ firstClassIncreased.addEventListener("click", function() {
     const economyClassPrice = getElementByIdWithInnerText("e-class-price");
     const getSubTotal = price + parseInt(economyClassPrice);
     const subTotal = getElementById("sub-total");
-    subTotal.innerHTML = getSubTotal;
-
-
+    subTotal.innerHTML = getSubTotal; //sub-total count
     let tax = getElementById("tax");
-    const taxTotal = getSubTotal * 0.10
+    const taxTotal = getSubTotal * 0.10; //tax count
     tax.innerHTML = taxTotal;
     const total = getElementById("total");
-    total.innerHTML = getSubTotal + taxTotal;
-
-
-
-
-
+    total.innerHTML = getSubTotal + taxTotal; //total count
 })
 
+// first class decreased button
 const firstClassDecreased = getElementById("decrease-f-class");
 firstClassDecreased.addEventListener("click", function() {
-    console.log("clicked")
-    count("f-class-count", false)
-
+    count("f-class-count", false); //count function false decreased by one
     const countForMultiply = getElementByIdWithValue("f-class-count");
     const price = 150 * countForMultiply;
     const updatePrice = getElementById("f-class-price")
@@ -41,52 +28,42 @@ firstClassDecreased.addEventListener("click", function() {
     const economyClassPrice = getElementByIdWithInnerText("e-class-price");
     const getSubTotal = price + parseInt(economyClassPrice);
     const subTotal = getElementById("sub-total");
-    subTotal.innerHTML = getSubTotal;
-
-
+    subTotal.innerHTML = getSubTotal; //sub-total count
     let tax = getElementById("tax");
-    const taxTotal = getSubTotal * 0.10
+    const taxTotal = getSubTotal * 0.10; //tax count
     tax.innerHTML = taxTotal;
     const total = getElementById("total");
-    total.innerHTML = getSubTotal + taxTotal;
+    total.innerHTML = getSubTotal + taxTotal; //total count
 })
 
 
-// economy class
+// economy class increased button
 const economyClassIncreased = document.getElementById('increase-e-class');
 economyClassIncreased.addEventListener("click", function() {
-
-
-    count("e-class-count", true);
-
-    const countForMultiply = getElementByIdWithValue("e-class-count");
-    const price = 100 * countForMultiply;
-    const updatePrice = getElementById("e-class-price")
-    updatePrice.innerHTML = price;
-    const fClassPrice = getElementByIdWithInnerText("f-class-price");
-    const getSubTotal = price + parseInt(fClassPrice);
-    const subTotal = getElementById("sub-total");
-    subTotal.innerHTML = getSubTotal;
-
-
-    let tax = getElementById("tax");
-    const taxTotal = getSubTotal * 0.10
-    tax.innerHTML = taxTotal;
-    const total = getElementById("total");
-    total.innerHTML = getSubTotal + taxTotal;
+        count("e-class-count", true); //count function true increased by one
+        const countForMultiply = getElementByIdWithValue("e-class-count");
+        const price = 100 * countForMultiply;
+        const updatePrice = getElementById("e-class-price")
+        updatePrice.innerHTML = price;
+        const fClassPrice = getElementByIdWithInnerText("f-class-price");
+        const getSubTotal = price + parseInt(fClassPrice);
+        const subTotal = getElementById("sub-total");
+        subTotal.innerHTML = getSubTotal; //sub-total count
+        let tax = getElementById("tax");
+        const taxTotal = getSubTotal * 0.10; //tax count
+        tax.innerHTML = taxTotal;
+        const total = getElementById("total");
+        total.innerHTML = getSubTotal + taxTotal; //total count
 
 
 
 
 
-})
-
-const economyClassDecreased = document.getElementById('decrease-e-class');
+    })
+    // economy class decreased button
+const economyClassDecreased = getElementById('decrease-e-class');
 economyClassDecreased.addEventListener("click", function() {
-
-
-    count("e-class-count", false);
-
+    count("e-class-count", false); //count function false decreased by one
     const countForMultiply = getElementByIdWithValue("e-class-count");
     const price = 100 * countForMultiply;
     const updatePrice = getElementById("e-class-price")
@@ -94,48 +71,36 @@ economyClassDecreased.addEventListener("click", function() {
     const fClassPrice = getElementByIdWithInnerText("f-class-price");
     const getSubTotal = price + parseInt(fClassPrice);
     const subTotal = getElementById("sub-total");
-    subTotal.innerHTML = getSubTotal;
-
-
+    subTotal.innerHTML = getSubTotal; //sub-total count
     let tax = getElementById("tax");
-    const taxTotal = getSubTotal * 0.10
+    const taxTotal = getSubTotal * 0.10; //tax count
     tax.innerHTML = taxTotal;
     const total = getElementById("total");
-    total.innerHTML = getSubTotal + taxTotal;
-
-
-
-
-
+    total.innerHTML = getSubTotal + taxTotal; //total count
 })
 
 // book now
 const bookBtn = getElementById("book-btn");
 bookBtn.addEventListener("click", function() {
-        const bookingArea = getElementById("booking-area");
-        if (getElementByIdWithInnerText("total") != 0) {
-            bookingArea.style.display = 'none';
-            const confirmation = getElementById("confirmation");
-            confirmation.style.display = 'block';
-        }
+    const bookingArea = getElementById("booking-area");
+    // if ticket are zero it show alert
+    if (getElementByIdWithInnerText("total") != 0) {
+        bookingArea.style.display = 'none';
+        const confirmation = getElementById("confirmation");
+        confirmation.style.display = 'block';
+    } else {
+        alert("Please provide valid information");
+    }
+    bookPopup("e-class-count", "e-class-price", "e-confirm-quantity", "e-confirm-amount"); //confirmation update function 
+    bookPopup("f-class-count", "f-class-price", "f-confirm-quantity", "f-confirm-amount"); //confirmation update function 
+    const total = getElementByIdWithInnerText("total");
+    const totalprice = getElementById("total-confirmed");
+    totalprice.innerHTML = total;
+})
 
-        // const equantity = getElementByIdWithValue("e-class-count");
-        // const ePrice = getElementByIdWithInnerText("e-class-price")
-        // console.log(equantity)
-        // console.log(ePrice)
-        // const eConfirmationQuantity = getElementById("e-confirm-quantity");
-        // eConfirmationQuantity.innerHTML = equantity;
-        // const eConfirmPrice = getElementById("e-confirm-amount");
-        // eConfirmPrice.innerHTML = ePrice;
-        bookPopup("e-class-count", "e-class-price", "e-confirm-quantity", "e-confirm-amount")
-        bookPopup("f-class-count", "f-class-price", "f-confirm-quantity", "f-confirm-amount")
-        const total = getElementByIdWithInnerText("total");
-        const totalprice = getElementById("total-confirmed");
-        totalprice.innerHTML = total;
-    })
-    // custom function
+// custom function
 
-// This function is for id count
+// This function is for class count
 function count(id, isIncrease) {
     const Count = document.getElementById(id).value;
     const CountNumber = parseInt(Count);
@@ -168,19 +133,7 @@ function getElementByIdWithInnerText(id) {
     return result;
 }
 
-
-// function total(classCountId,tiketPrice,workingClassPriceId,differClassPriceId){
-//     const countForMultiply = getElementByIdWithValue("e-class-count");
-//     const price = 100 * countForMultiply;
-//     const updatePrice = getElementById("e-class-price")
-//     updatePrice.innerHTML = price;
-//     const fClassPrice = getElementByIdWithInnerText("f-class-price");
-//     const getSubTotal = price + parseInt(fClassPrice);
-//     const subTotal = getElementById("sub-total");
-//     subTotal.innerHTML = getSubTotal;
-// }
-
-// Showing booked information
+//function for Showing booked confirmation
 function bookPopup(classCountId, classPriceId, bookQuantityId, bookAmountId) {
     const quantity = getElementByIdWithValue(classCountId);
     const Price = getElementByIdWithInnerText(classPriceId)
